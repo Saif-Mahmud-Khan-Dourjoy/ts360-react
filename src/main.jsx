957f3dedda610872545ca960064cb2ca24a31f12
Saif-Mahmud-Khan-Dourjoy <60444138+Saif-Mahmud-Khan-Dourjoy@users.noreply.github.com>
2024-07-08 22:20:47 +0600

    fixed issues

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter  as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { HelmetProvider } from "react-helmet-async";
+import {  HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
